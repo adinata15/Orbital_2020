b87477d0d4f00ccce70732a2c704f1c63e7a4335
cococrunch <adinatatan99@gmail.com>
2020-06-18 10:31:15 +0800

    remove breadcrumbs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
 				<Payment />
 				<NavBar />
 				<Carousel />
-				<Breadcrumbs />
+				{/* <Breadcrumbs /> */}
 				<ShopBar />
 				<FooterBar />
 			</div>
