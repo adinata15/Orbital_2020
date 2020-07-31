4aa62255e2a0cfa221d94cdd51e0d7921f671da1
samtjong <samtjong@email.com>
2020-08-01 00:33:00 +0800

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -19,14 +20,22 @@ app.use((req, res, next) => {
   }
 });
 
-app.get('/', (req, res) => res.send('API running'));
-
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/items', require('./routes/api/items'));
 app.use('/api/stripe', require('./routes/api/stripe'));
 app.use('/api/size-assistant', require('./routes/api/size-assistant'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
